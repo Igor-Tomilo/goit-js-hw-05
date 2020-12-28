ccda3a3ac5fdc4bcf2aeccfc361184128d49e0b9
Igor <70631855+Igor-Tomilo@users.noreply.github.com>
2020-12-28 08:35:57 +0200

    .

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,11 +1,12 @@
 // Напиши функцию-конструктор Account, которая создает объект со свойствами login и email. В prototype функции-конструктора добавь метод getInfo(), который выводит в консоль значения полей login и email объекта который его вызвал.
-const Account = function(login, email) {
-  this.login = login;
-  this.email = email;
-}
-
-Account.prototype.getInfo = function() {
-  console.log(`login: ${this.login}, email: ${this.email}`);
+class Account {
+  constructor({ login, email }) {
+    this.login = login;
+    this.email = email;
+  }
+  getInfo() {
+    console.log(`login: ${this.login}, email: ${this.email}`);
+  }
 }
 
 console.log(Account.prototype.getInfo); // function
--- JS/task-2.js
+++ JS/task-2.js
@@ -7,15 +7,13 @@
 // Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
 class User {
   constructor(user) {
-      this.name = user.name;
-      this.age = user.age;
-      this.followers = user.followers;
+    this.name = user.name;
+    this.age = user.age;
+    this.followers = user.followers;
   }
 
   getInfo() {
-      return `User ${this.name} is ${this.age} years old and has ${
-          this.followers
-      } followers`;
+    return `User ${this.name} is ${this.age} years old and has ${this.followers} followers`;
   }
 }
 
@@ -25,7 +23,7 @@ const mango = new User({
   followers: 20,
 });
 
-mango.getInfo(); // User Mango is 2 years old and has 20 followers
+console.log(mango.getInfo()); // User Mango is 2 years old and has 20 followers
 
 const poly = new User({
   name: 'Poly',
@@ -33,4 +31,4 @@ const poly = new User({
   followers: 17,
 });
 
-poly.getInfo(); // User Poly is 3 years old and has 17 followers
+console.log(poly.getInfo()); // User Poly is 3 years old and has 17 followers
